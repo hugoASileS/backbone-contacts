0f82d7c456a5ef186c97157e5fa57534944da0a0
Hugo Siles <sisahugo@gmail.com>
2022-05-16 19:38:17 -0500

    feat: all routes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,50 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import App from "./App";
+import { renderWithRouter } from "./utils/tests";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hola mundo/i);
-  expect(linkElement).toBeInTheDocument();
+describe("all main routes should be shown", () => {
+  test("Home page", () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  });
+  test("Contacts page", () => {
+    renderWithRouter(<App />, { route: "/contacts" });
+    expect(screen.getByText(/Contacts/i)).toBeInTheDocument();
+  });
+  test("Email page", () => {
+    renderWithRouter(<App />, { route: "/email" });
+    expect(screen.getByText(/Email/i)).toBeInTheDocument();
+  });
+  test("User page", () => {
+    renderWithRouter(<App />, { route: "/user" });
+    expect(screen.getByText(/User/i)).toBeInTheDocument();
+  });
+  test("Role page", () => {
+    renderWithRouter(<App />, { route: "/role" });
+    expect(screen.getByText(/Role/i)).toBeInTheDocument();
+  });
+});
+
+describe("contacts", () => {
+  const contactId = 12345;
+  test("Update page", () => {
+    renderWithRouter(<App />, { route: `/contacts/${contactId}/update` });
+    expect(
+      screen.getByText(new RegExp(`${contactId}`, "i"))
+    ).toBeInTheDocument();
+  });
+  test("Delete page", () => {
+    renderWithRouter(<App />, { route: `/contacts/${contactId}/delete` });
+    expect(
+      screen.getByText(new RegExp(`${contactId}`, "i"))
+    ).toBeInTheDocument();
+  });
+  test("Create page", () => {
+    renderWithRouter(<App />, { route: `/contacts/create` });
+    expect(screen.getByText(/Create contact/i)).toBeInTheDocument();
+  });
+  test("List page", () => {
+    renderWithRouter(<App />, { route: `/contacts` });
+    expect(screen.getByText(/List Contact/i)).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
-function App() {
-  return <div>Hola mundo</div>;
+import { Routes, Route, Outlet, useParams } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/contacts" element={<ContactsContainer />}>
+        <Route path="" element={<ListContact />} />
+        <Route path=":contactId/update" element={<UpdateContact />} />
+        <Route path="create" element={<CreateContact />} />
+        <Route path=":contactId/delete" element={<DeleteContact />} />
+      </Route>
+      <Route path="/email" element={<h1>Email</h1>} />
+      <Route path="/file" element={<h1>File</h1>} />
+      <Route path="/user" element={<h1>User</h1>} />
+      <Route path="/role" element={<h1>Role</h1>} />
+    </Routes>
+  );
 }
 
-export default App;
+function ContactsContainer() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+function ListContact() {
+  return <h1>List Contact</h1>;
+}
+
+function UpdateContact() {
+  let { contactId } = useParams();
+  return <h1>Update contact {contactId}</h1>;
+}
+
+function CreateContact() {
+  return <h1>Create contact</h1>;
+}
+
+function DeleteContact() {
+  let { contactId } = useParams();
+  return <h1>Delete contact {contactId}</h1>;
+}
